6089c76d688200b06f68e359f4706f0e660800b7
Sathish <sathish.t2000@gmail.comm>
2020-10-23 22:01:50 +0530

    hour glass problem

--- hourglass.py
+++ hourglass.py
@@ -1,9 +1,45 @@
-a= [[1,2,3],
-    [4,5,6],
-    [7,8,9]]
-b=0
-for i in a:
-    b =b+ sum(i)
-b = b - a[1][0] - a[1][2]
+#!/bin/python3
 
-print(b)
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def cal(arr):
+    b = 0
+    for i in arr:
+        b =b+ sum(i)
+    b = b - arr[1][0] - arr[1][2]
+    return b
+    
+
+def hourglassSum(arr):
+    final = []
+    for j in range(0,4):
+        for k in range(0,4):
+            lst = []
+            for i in range(0,3):
+                lst.append(arr[i+k][0+j:3+j])
+            value = cal(lst)
+            final.append(value)
+
+                
+
+    return max(final)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
